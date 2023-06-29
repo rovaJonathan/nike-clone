09cb583f127d2cbdb7eb78b7e23facc25568c17b
Jonathan <jonathan@digital.gov.mg>
2023-06-29 12:43:02 +0300

    Add filter button

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -3,7 +3,7 @@ import ItemProduct from "./ItemProduct";
 
 const Body: FC = () => {
   return (
-    <div className="flex-1 grid grid-cols-2 md:grid-cols-3 gap-2">
+    <div className="flex-1 grid grid-cols-2 md:grid-cols-3 gap-2 overflow-scroll max-h-full md:max-h-screen">
       <ItemProduct imageUrl="pg-5.jpg" />
       <ItemProduct imageUrl="pg-5.jpg" />
       <ItemProduct imageUrl="pg-5.jpg" />
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,10 +2,27 @@ import React, { FC } from "react";
 
 const Header: FC = () => {
   return (
-    <div>
-      <h1 className="text-1xl font-semibold py-5">
+    <div className="flex flex-row justify-between items-center py-5 px-5 md:px-0">
+      <h1 className="text-2xl md:text-1xl font-semibold ">
         Nouveautés Hommes €100 - €150 (23)
       </h1>
+      <div className="ml-10 md:hidden block cursor-pointer">
+        <svg
+          className="w-[21px] h-[21px] text-gray-800"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 20 20"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="1.5"
+            d="M7.75 4H19M7.75 4a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 4h2.25m13.5 6H19m-2.25 0a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 10h11.25m-4.5 6H19M7.75 16a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 16h2.25"
+          />
+        </svg>
+      </div>
     </div>
   );
 };
